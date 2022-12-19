dc83efd917c6d5be34b9a2e91d4537b750e75de2
Judson Paiva <judsonpaiva@Judsons-MacBook-Pro.local>
2022-12-19 22:00:27 +0100

    Pagina detalhes do Filme

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,10 +1,57 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../service/api";
 
 function Filme() {
-    return (
-      <div>
-        <h1>Filme</h1>
+
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadingFilme() {
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: 'd6a798909e4d30b9ae3b40401db36827',
+          language: "pt-BR"
+        }
+      })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("NAo encontrado");
+        })
+    }
+
+    loadingFilme();
+
+
+    return ()=>{
+      console.log("Componente desmontado");
+    }
+
+  }, []);
+
+
+  if(loading){
+    return(
+      <div className='filme-info'>
+        <h2>Carregando Detalhes</h2>
       </div>
     );
   }
-  
-  export default Filme;
+
+  return (
+    <div className='filme-info'>
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliaçao: {filme.vote_average}</strong>
+    </div>
+  );
+}
+
+export default Filme;
