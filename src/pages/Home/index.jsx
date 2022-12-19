ab5698f4671105daf1da71900af4f266f1d12b39
Judson Paiva <judsonpaiva@Judsons-MacBook-Pro.local>
2022-12-19 20:15:59 +0100

    Listando Filmes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,8 @@
 import {useEffect, useState} from 'react';
 import api from '../../service/api';
+import { Link } from 'react-router-dom';
+import './home.css';
+
 
 function Home() {
 
@@ -15,15 +18,32 @@ function Home() {
           page: 1
         }
       });
-      console.log(response.data.results);
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10));
     }
+
+
     loadFilmes();
+
+
   }, []);
 
 
     return (
-      <div>
-        <h1>Home</h1>
+      <div className='container'>
+        <div className='lista-filmes'>
+          {
+            filmes.map((filme)=>{
+              return (
+                <article key={filme.id}>
+                  <strong>{filme.title}</strong>
+                  <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                  <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                </article>
+              );
+            })
+          }
+        </div>
       </div>
     );
   }
